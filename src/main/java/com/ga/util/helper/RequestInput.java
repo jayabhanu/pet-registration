1206f0c3aaf7f50fd4d5987c7db3670148fc2aaa
jayabhanu <jayabhanubec@gmail.com>
2016-03-31 21:42:20 -0700

    add helper

---
+++ src/main/java/com/ga/util/helper/RequestInput.java
@@ -0,0 +1,137 @@
+/**
+ *   File Name: RequestInput.java<br>
+ *
+ *   Gajula, jayabhanu<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: Mar 30, 2016
+ *
+ */
+
+package com.ga.util.helper;
+
+import java.util.*;
+
+/**
+ * RequestInput //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ *
+ * @author LastName, FirstName
+ * @version 1.0.0
+ * @since 1.0
+ *
+ */
+public class RequestInput {
+	private static String[] acceptableNumber;
+	private static Scanner scanner;
+
+	// Get a char value from the user
+	public static char getChar(String question) {
+		return 0;
+
+	}
+
+	// Get a char value from the user, must be an acceptable value
+	public static char getChar(String question, char... acceptWord) {
+		int count = 0;
+		System.out.println(question);
+		String inputword = scanner.nextLine();
+		for (int i = 0; i <= inputword.length(); i++) {
+			if (acceptWord[i] == 'a' || acceptWord[i] == 'e' || acceptWord[i] == 'i' || acceptWord[i] == 'o'
+					|| acceptWord[i] == 'u') {
+				count++;
+			}
+		}
+		System.out.println("They are:" + count + "owvals in this word");
+		return 'r';
+
+	}
+
+	// Get a double value from the user
+	public static double getDouble(String question) {
+		return 0;
+
+	}
+
+	// Get a int from the user, must be in acceptable range value
+	public static int getInt(String question, int... acceptableNumber) {
+		int resultValue;
+		String input = null;
+		boolean validInt = false;
+		scanner = new Scanner(System.in);
+		while (true) {
+			try {
+				System.out.println(question);
+				input = scanner.nextLine();
+				resultValue = Integer.parseInt(input);
+				for (int i = 0; i < acceptableNumber.length; i++) {
+					if (resultValue == acceptableNumber[i]) {
+						validInt = true;
+					}
+				}
+				if (!validInt) {
+
+					throw new InvalidAcceptableNumberException();
+				}
+				return resultValue;
+
+				// scanner.close();
+				// return resultValue;
+			} catch (NumberFormatException e) {
+				System.out.println("You have nott provided a valid type (" + input + ")");
+
+			} catch (InvalidAcceptableNumberException e) {
+				System.out.println("You have not an acceptable number (" + input + ")");
+			}
+		}
+	}
+
+	// Get a String from the user, must be an acceptable value
+	public static String getString(String question, String... acceptableWords) {
+		String resultValue;
+		String input = null;
+		boolean validWord = false;
+		scanner = new Scanner(System.in);
+		while (true) {
+			try {
+				System.out.println(question);
+				input = scanner.nextLine();
+				resultValue = input;
+				for (int i = 0; i < acceptableWords.length; i++) {
+					// just play around instead of trim(), toUppercase(),and
+					// trim().replace(" ", "").equals()
+					if (resultValue.trim().replace(" ", "").equalsIgnoreCase(acceptableWords[i])) {
+						validWord = true;
+					}
+				}
+				if (!validWord) {
+
+					throw new InvalidAcceptableWordException();
+				}
+				return resultValue;
+
+				// scanner.close();
+				// return resultValue;
+			} catch (InvalidAcceptableWordException e) {
+				System.out.println("You have not an acceptable word (" + input + ")");
+			}
+		}
+	}
+
+	// }
+	// String input;
+	// String resultValue;
+	// scanner = new Scanner(System.in);
+	// System.out.print(question);
+	// input = scanner.nextLine();
+	// resultValue = input;
+	// return resultValue;
+	//
+	// }
+
+	// Create your own helper method that you may find useful
+
+}
